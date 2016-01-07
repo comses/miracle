d5cbe68a741ad962a8757b7844e6eb599f811241
Allen Lee <allen.lee@asu.edu>
2016-01-07 15:51:39 -0700

    fix copy/replace typo

--- django/miracle/core/urls.py
+++ django/miracle/core/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     # FIXME: replace these with forms if needed
-    url(r'^contact/$', RedirectView.as_view(url='https://_groups.google.com/forum/#!forum/comses-dev', permanent=False),
+    url(r'^contact/$', RedirectView.as_view(url='https://groups.google.com/forum/#!forum/comses-dev', permanent=False),
         name='contact'),
     url(r'^report-bug/$', RedirectView.as_view(url='https://github.com/comses/miracle/issues/new', permanent=False),
         name='report_bug'),
